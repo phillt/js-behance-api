bed48719c22d2fb84eeaaff947b187925436025e
Felipe Tadeo <powerphillg5@gmail.com>
2018-07-26 11:37:30 -0500

    create react router dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {HashRouter as Router, Route} from "react-router-dom";
+import Home from "./screens/home/Home.js";
+import Profile from "./screens/profile/Profile.js";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <dizv className="App">
+                    Router.
+                    <Route exact path={"/"} component={Home}/>
+                    <Route exact path={"/profile"} component={Profile}/>
+                </dizv>
+            </Router>
+        );
+    }
 }
 
 export default App;
